4ff3de412b6a1acd6f1c53e4fc4d702026d9352e
adnan <adnan@DESKTOP-S6NSO22>
2021-12-19 13:34:50 -0500

    testng 12/15/21

--- Automationtraining/src/test/java/PageObjectModel/SignInAmazon.java
+++ Automationtraining/src/test/java/PageObjectModel/SignInAmazon.java
@@ -2,15 +2,26 @@ package PageObjectModel;// 12/15/21
 
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
+
+import Common.Utility;
+
 import org.testng.annotations.BeforeMethod;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.io.FileHandler;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 
-public class SignInAmazon {
-	public static WebDriver driver;
+public class SignInAmazon extends Utility {
+	
 	@Test(priority=1)
   public void f() throws InterruptedException {
 		Home ob=new Home(driver);
@@ -25,15 +36,18 @@ public class SignInAmazon {
   }
 	
 	@Test(priority=2)
-	public void VerificationOfText() throws InterruptedException {
+	public void VerificationOfText() throws InterruptedException, IOException {
 	Home ob=new Home(driver);
 	ob.SignToClick();
+	shots();
 	Thread.sleep(3000);
 	SignInPage it=new SignInPage(driver); //We are creating an object to call it from its class 
 	Thread.sleep(3000);
 	it.EmailFieldKeys();
 	Thread.sleep(3000);
+	shots();
 	it.ContinueButtonClick();
+	shots();
 	String errorit=it.text();
 	System.out.println(errorit);
 	SoftAssert soft=new SoftAssert();
@@ -41,8 +55,9 @@ public class SignInAmazon {
 	soft.assertEquals(errorit, "We cannot find an account with that email address");
 	System.out.println("This is after assertion");
 	}
+}
 	
-  @BeforeMethod
+ /* @BeforeMethod
   public void beforeMethod() {
 	 browsers("chrome","https://www.amazon.com/");
 	  
@@ -54,13 +69,15 @@ public class SignInAmazon {
 	  driver.quit();
   }
   public void browsers(String browser, String wbsite) {
+	  String local=System.getProperty("user.dir");
+	  System.out.println("local");
 	  if(browser.equalsIgnoreCase("Chrome")){
-		  System.setProperty("webdriver.chrome.driver", "C:\\Users\\adnan\\eclipse-workspace\\Automationtraining\\src\\Drivers\\chromedriver.exe");
+		  System.setProperty("webdriver.chrome.driver", local+"\\src\\Drivers\\chromedriver.exe");
 		  driver=new ChromeDriver();
 		  driver.navigate().to(wbsite);
 		  driver.manage().window().maximize();
 	   }else if(browser.equalsIgnoreCase("Edge")){
-		  System.setProperty("webdriver.edge.driver", "C:\\Users\\adnan\\git\\repository\\Automationtraining\\src\\Drivers\\msedgedriver.exe");
+		  System.setProperty("webdriver.edge.driver", local+"C\\src\\Drivers\\msedgedriver.exe");
 			  driver=new EdgeDriver();  
 			  driver.navigate().to(wbsite);
 			  driver.manage().window().maximize();
@@ -68,5 +85,15 @@ public class SignInAmazon {
 		 System.out.println("Correct browser name");
 	  }
   }
+  public void shots() throws IOException { //   12/15/21
+	  Date dt= new Date();
+	  System.out.println(dt);
+	  String it=dt.toString().replace(" ", "_").replace(":", "_");
+	  System.out.println(it);
+	  String local=System.getProperty("user.dir");
+	  File stored=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+	  FileHandler.copy(stored,new File(local+"\\Picture\\"+it+"captured.jpeg"));
+	  
+  }
 
-}
+} */
---
+++ Automationtraining/src/test/java/PageObjectModel/SignInAmazon2.java
@@ -0,0 +1,95 @@
+package PageObjectModel;// 12/15/21
+
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import org.testng.annotations.BeforeMethod;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+
+public class SignInAmazon2 {
+	public static WebDriver driver;
+	@Test(priority=1)
+  public void f() throws InterruptedException {
+		Home ob=new Home(driver);
+		ob.SignToClick();
+		Thread.sleep(3000);
+		SignInPage it=new SignInPage(driver); //We are creating an object to call it from its class 
+		Thread.sleep(3000);
+		it.EmailFieldKeys();
+		Thread.sleep(3000);
+		it.ContinueButtonClick();
+		
+  }
+	
+	@Test(priority=2)
+	public void VerificationOfText() throws InterruptedException, IOException {
+	Home ob=new Home(driver);
+	ob.SignToClick();
+	shots();
+	Thread.sleep(3000);
+	SignInPage it=new SignInPage(driver); //We are creating an object to call it from its class 
+	Thread.sleep(3000);
+	it.EmailFieldKeys();
+	Thread.sleep(3000);
+	shots();
+	it.ContinueButtonClick();
+	shots();
+	String errorit=it.text();
+	System.out.println(errorit);
+	SoftAssert soft=new SoftAssert();
+	soft.assertAll();
+	soft.assertEquals(errorit, "We cannot find an account with that email address");
+	System.out.println("This is after assertion");
+	}
+	
+  @BeforeMethod
+  public void beforeMethod() {
+	 browsers("chrome","https://www.amazon.com/");
+	  
+  }
+
+  @AfterMethod
+  public void afterMethod() throws InterruptedException {
+	  Thread.sleep(3000);
+	  driver.quit();
+  }
+  public void browsers(String browser, String wbsite) {
+	  String local=System.getProperty("user.dir");
+	  System.out.println("local");
+	  if(browser.equalsIgnoreCase("Chrome")){
+		  System.setProperty("webdriver.chrome.driver", local+"\\src\\Drivers\\chromedriver.exe");
+		  driver=new ChromeDriver();
+		  driver.navigate().to(wbsite);
+		  driver.manage().window().maximize();
+	   }else if(browser.equalsIgnoreCase("Edge")){
+		  System.setProperty("webdriver.edge.driver", local+"C\\src\\Drivers\\msedgedriver.exe");
+			  driver=new EdgeDriver();  
+			  driver.navigate().to(wbsite);
+			  driver.manage().window().maximize();
+	  } else {
+		 System.out.println("Correct browser name");
+	  }
+  }
+  public void shots() throws IOException { //   12/15/21
+	  Date dt= new Date();
+	  System.out.println(dt);
+	  String it=dt.toString().replace(" ", "_").replace(":", "_");
+	  System.out.println(it);
+	  String local=System.getProperty("user.dir");
+	  File stored=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+	  FileHandler.copy(stored,new File(local+"\\Picture\\"+it+"captured.jpeg"));
+	  
+  }
+
+}
